2de4335e3f9ac281ba74e3eb204b82a0fb9257cc
SURENTHARKUMAR843 <surentharkumarsk843@gmail.com>
2023-09-21 19:33:56 +0100

    string

---
+++ string.js
@@ -0,0 +1,14 @@
+let string = "surenthar kumar "
+
+
+// string is "  "  dobule quteson in string....
+
+// string is '  ' single quteson in string .. 
+
+// example ..
+
+
+
+//  " surenthar"  "22"
+
+//  'kumar'    '25'
