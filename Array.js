46050405bcb773a810991a0f49a8f987078191ac
SURENTHARKUMAR843 <surentharkumarsk843@gmail.com>
2023-09-21 17:42:37 +0100

    Array

---
+++ Array.js
@@ -0,0 +1,25 @@
+let arr =[ 1,2,3,4,5,6,7,8,9,10]
+
+
+// array pracket == [ ]
+
+
+console.log(arr)
+function array(){
+    for(i in arr){
+        console.log(i)
+    }
+    
+};
+array()
+
+
+//  String of array ..
+
+let arr2 =["surenthar","kumar","arun","mani","ashok","sanjai"];
+console.log(arr2)
+
+for (i in arr2){
+    console.log(i)
+}
+
